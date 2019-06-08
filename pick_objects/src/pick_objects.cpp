1b34ff5af43b23b3d5ddb29591028b7070a05ebd
root <music0and0wine@gmail.com>
2019-06-08 15:23:13 +0000

    Add 5 second pose in pick objects

--- pick_objects/src/pick_objects.cpp
+++ pick_objects/src/pick_objects.cpp
@@ -29,39 +29,54 @@ int main(int argc, char** argv){
   goal.target_pose.pose.orientation.w = 1.0;
 
    // Send the goal position and orientation for the robot to reach
-  ROS_INFO("Sending goal");
+  ROS_INFO("Sending 1st goal in Pick-up zone");
   ac.sendGoal(goal);
 
   // Wait an infinite time for the results
   ac.waitForResult();
 
   // Check if the robot reached its goal
-  if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+  if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED){
     //ROS_INFO("Hooray, the base moved 1 meter forward");
     ROS_INFO("Successfully to reach 1st Goalin Pich-up zone");
-  else
+  }else{
     ROS_INFO("The base failed to move forward 1 meter for some reason");
+  }
 
+  // Wait next action
+  ros::Rate rate(10);
+  ros::Time start = ros::Time::now();
+  ros::Time end = ros::Time::now();
+  while(ros::ok()){
+    end = ros::Time::now();
+    if((start - end) > ros::Duration(5.0f)){
+      break;
+    }
+    ros::spinOnce();
+  }
 
-  
+  // set up the frame parameters
+  goal.target_pose.header.frame_id = "map";
+  goal.target_pose.header.stamp = ros::Time::now();
+
   // Define a position and orientation for the robot to reach
   goal.target_pose.pose.position.x = 2.0;
   goal.target_pose.pose.orientation.w = 0.0;
 
    // Send the goal position and orientation for the robot to reach
-  ROS_INFO("Sending goal");
+  ROS_INFO("Sending 2nd goal in Drop-Off zone");
   ac.sendGoal(goal);
 
   // Wait an infinite time for the results
   ac.waitForResult();
 
   // Check if the robot reached its goal
-  if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+  if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED){
     //ROS_INFO("Hooray, the base moved 1 meter forward");
-    ROS_INFO("Successfully to reach 2st Goal in Drop-Off zone");
-  else
+    ROS_INFO("Successfully to reach 2nd Goal in Drop-Off zone");
+  }else{
     ROS_INFO("The base failed to move forward 1 meter for some reason");
-
+  }
   
   return 0;
 }
